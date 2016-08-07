19ee66fc7cbb4e35ac325a4a6e57d1dfae7022ee
pinodex <raphmarco@hotmail.ph>
2016-08-08 01:26:22 +0800

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using UNIARS.Auth;
+
+namespace UNIARS
+{
+    public partial class App : Application
+    {
+        public static User User = new User();
+
+        public App()
+        {
+            
+        }
+    }
+}
---
+++ Auth/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UNIARS.Auth
+{
+    public class User
+    {
+        public string Name { get; protected set; }
+
+        public User()
+        {
+            this.Name = "Unknown User";
+        }
+
+        public User(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
